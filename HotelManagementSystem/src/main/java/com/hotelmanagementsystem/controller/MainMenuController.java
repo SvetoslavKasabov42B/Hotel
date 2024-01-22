e64de343c169a8758c26bea434e7b0c9baced379
VladimirF <Vladofilchev@gmail.com>
2024-01-22 19:48:18 +0200

    Main menu added

---
+++ HotelManagementSystem/src/main/java/com/hotelmanagementsystem/controller/MainMenuController.java
@@ -0,0 +1,82 @@
+package com.hotelmanagementsystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+public class MainMenuController {
+
+    public HBox hbox1;
+    @FXML
+    private Button checkIn;
+
+    @FXML
+    private Button checkOut;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private Label mainMenu;
+
+    @FXML
+    private Button monthlyReport;
+
+    @FXML
+    private Button rackRoom;
+
+    @FXML
+    private Button reservations;
+
+    @FXML
+    private Label reservationsCount;
+
+    @FXML
+    private Pane root;
+
+    @FXML
+    private Label todayDate;
+
+    @FXML
+    private Label todayGuests;
+
+    @FXML
+    private Button userManagement;
+
+    @FXML
+    private Label wellcomeMSG;
+
+    @FXML
+    void openCheckInMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openCheckOutMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openMonthlyReportMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRackRoomMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openReservationsMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openUserManagementMenu(ActionEvent event) {
+
+    }
+
+}
